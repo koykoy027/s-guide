772083895591e710318c06f174db8f33b46147c2
Zharzha <Deadth03@gmail.com>
2023-03-05 00:22:36 +0800

    lorem updates

--- resources/views/Homepage/home.blade.php
+++ resources/views/Homepage/home.blade.php
@@ -921,7 +921,7 @@ As early as in pre-Magellanic times, education was informal, unstructured, and d
                             <a href="#">
                                 <img src="lp/lp/assets/images/123.png" alt="Logo">
                             </a>
-                            <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, repudiandae! Totam, nemo sed? Provident.</p> <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus</p>
+                            <p class="text"></p> <p class="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         </div> <!-- footer about -->
                     </div>
                     <div class="col-lg-3 col-sm-6 order-sm-3 order-lg-2">
