936049ab50938404201da7af2e6a0c0537bec499
Josh <villanuevajoshua27@gmail.com>
2023-03-12 19:07:29 +0800

    online reporting

--- resources/views/Homepage/online-reporting.blade.php
+++ resources/views/Homepage/online-reporting.blade.php
@@ -1,107 +1,191 @@
 @extends('adminLayout.app')
+@section('title', '| Online Reporting')
 @section('content')
-    
 
-    
-         <div class="container mt-3 mb-3">
- 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
- 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>{{ $message }}</strong>
-        </div>
-    @endif
-    
-    <h3>Complain Form</h3>
-    <form action="{{ route('online-reporting') }}" method="POST" enctype="multipart/form-data">
- 
-        @csrf
-    
-        <div class="form-group">
-            <label>Complainant</label>
-            <select class="form-control" name="title" id="title">
-                <option value="">--</option>
-                <option value="mr">Student</option>
-                <option value="ms.">Guardian</option>
-            </select>
-        </div>
 
-         <div class="form-group">
-            <label>School ID</label>
-            <select class="form-control" name="title" id="title">
-                <option value="">--</option>
-                <option value="TalaHighSchool">305380</option>
-                <option value="UrdujaELementary">136638</option>
-            </select>
-        </div>
- 
-        <div class="form-group">
-            <label>Name</label>
-            <input type="text" class="form-control" name="name" id="name">
-        </div>
- 
-        <div class="form-group">
-            <label>Date of Incidence</label>
-            <input type="date" class="form-control" name="bday" id="bday">
-        </div>
- 
-        <div class="form-group">
-            <label>Age</label>
-            <input type="number" class="form-control" name="age" id="age">
-        </div>
- 
-        <div class="form-group">
-            <label>Gender</label>
-            <select class="form-control" name="title" id="title">
-                <option value="">--</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-            </select>
-        </div>
- 
-        <div class="form-group">
-            <label>Phone</label>
-            <input type="text" class="form-control" name="phone" id="phone">
-        </div>
- 
-        <div class="form-group">
-            <label>Statemet</label>
-            <textarea class="form-control" name="address" id="address" rows="4"></textarea>
-        </div>
- 
-        <div class="form-group">
-            <label>Email</label>
-            <input type="email" class="form-control" name="email" id="email">
-        </div>
- 
-        <div class="form-group">
-            <label>Password</label>
-            <input type="password" class="form-control" name="password" id="password">
-        </div>
- 
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="1" id="t&c" name="t&c">
-                <label class="form-check-label">
-                    I agree to the terms and conditions.
-                </label>
+
+    <div class="this">
+        <div class="container my-5 shadow-lg" style="background: #fff">
+            <div class="row">
+                <form class="p-5" method="" action="">
+                    @csrf
+
+                    {{-- <div class="d-flex justify-content-center">
+                        <img src="{{ asset('images/SG 2.png') }}" alt="S-GUIDE">
+                    </div> --}}
+
+                    <h4>Online Reporting</h4>
+                    <small><em>Note: Please input all fields.</em></small>
+
+                    <div class="row my-3">
+                        <h6>School information</h6>
+                        <div class="col-md">
+                            <input type="text" placeholder="School code"
+                                class="form-control @error('school_id') is-invalid @enderror" name="school_id"
+                                value="{{ old('school_id') }}" required autocomplete="school_id" autofocus>
+                            @error('school_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="col-md">
+                            <input type="text" placeholder="Student number"
+                                class="form-control @error('employee_number') is-invalid @enderror" name="employee_number"
+                                value="{{ old('employee_number') }}" required autocomplete="employee_number" autofocus>
+                            @error('employee_number')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md">
+                            <select name="complainant" class="form-control" onchange="selectComplainant(this.value)">
+                                <option value="Student">Student</option>
+                                <option value="Parent / Guardian">Parent / Guardian</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3" name="others_complainant" style="display: none">
+                        <div class="col-md">
+                            <input type="text" class="form-control" name="guardian_name"
+                                placeholder="Name Parent / Guardian">
+                        </div>
+                        <div class="col-md">
+                            <input type="text" class="form-control" name="guardian_relationship"
+                                placeholder="Relationship">
+                        </div>
+                    </div>
+
+                    <hr />
+                    <div class="row">
+                        <h6>Personal information</h6>
+                        <div class="col-md mb-3">
+                            <input placeholder="Last Name" type="text"
+                                class="form-control @error('lastname') is-invalid @enderror" name="lastname"
+                                value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
+                            @error('lastname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="col-md mb-3">
+                            <input placeholder="First Name" type="text"
+                                class="form-control @error('firstname') is-invalid @enderror" name="firstname"
+                                value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
+                            @error('firstname')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="col-md mb-3">
+                            <input placeholder="M.I" type="text" class="form-control @error('MI') is-invalid @enderror"
+                                name="MI" value="{{ old('MI') }}" required autocomplete="MI" autofocus>
+                            @error('MI')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md">
+                            <select class="form-control @error('gender') is-invalid @enderror" value="{{ old('gender') }}"
+                                required autocomplete="gender" autofocus" name="gender">
+                                <option selected disabled>Select gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                            @error('gender')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md">
+                            <input placeholder="Email Address" type="email"
+                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mb-3">
+                        <div class="col-md">
+                            <h6>Report Details</h6>
+                            <select class="form-control @error('type_of_complain') is-invalid @enderror"
+                                value="{{ old('type_of_complain') }}" autocomplete="on" autofocus name="type_of_complain"
+                                onchange="selectTypesOfComplain(this.value)">
+                                <option value="Bullying">Bullying</option>
+                                <option value="Sexual Harassment">Sexual Harassment</option>
+                                <option value="Thievery">Thievery</option>
+                                <option value="Others">Others</option>
+                            </select>
+
+                            @error('type_of_complain')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <div class="col-md my-3">
+                                <input type="text" class="form-control" name="others_type_of_complain"
+                                    placeholder="Please specify" style="display: none">
+                            </div>
+                        </div>
+
+                        <div class="col-md mb-3">
+                            <h6>Place of Incidence</h6>
+                            <input type="text" class="form-control @error('place_of_incidence') is-invalid @enderror"
+                                value="{{ old('place_of_incidence') }}" autocomplete="on"
+                                autofocusname="place_of_incidence" name="place_of_incidence">
+                            @error('place_of_incidence')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md">
+                                <h6>Summary</h6>
+                                <textarea class="form-control @error('summary') is-invalid @enderror" placeholder="Tell me more"
+                                    value="{{ old('summary') }}" autocomplete="on" autofocusname="status" cols="30" rows="10"
+                                    name="summary"></textarea>
+                                @error('summary')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                        </div>
+                        <hr>
+
+                        <div class="d-flex justify-content-between">
+                            <a href="{{ url('online-portal') }}" class="btn btn-outline-secondary btn-sm">
+                                Back
+                            </a>
+                            <button type="submit" class="btn btn-primary btn-sm">
+                                Next
+                            </button>
+                        </div>
+                </form>
             </div>
         </div>
- 
-        <input type="reset" name="reset" value="Reset" class="btn btn-dark">
-        <input type="submit" name="submit" value="Submit" class="btn btn-dark">
-    
-    </form>
-
+    </div>
 
 @endsection
--- resources/views/adminLayout/app.blade.php
+++ resources/views/adminLayout/app.blade.php
@@ -13,31 +13,54 @@
     <!-- Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    {{-- custom js --}}
+    <script src="{{ asset('assets/dist/js/script.js') }}"></script>
+    {{-- JQUERY CDN --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
     <style>
-        main {
-            height: 100vh;
+        body {
+
             background: rgb(58, 218, 234);
             background: radial-gradient(circle, rgba(58, 218, 234, 1) 0%, rgba(37, 182, 213, 1) 50%, rgba(13, 139, 188, 1) 100%);
-            /* overflow: hidden; */
+
+
         }
+
 
         .transparent {
             background: rgba(0, 0, 0, 0.3);
-
             color: #fff;
             padding: 3% 30%;
             border-radius: 10px;
+        }
 
-
+        sup {
+            color: red;
+            text-transform: bold;
         }
     </style>
 </head>
 
 <body>
-    <main class="py-5 shadow-lg">
+    <div class="navbar-area headroom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg sticky-top">
+                        <a class="navbar-brand nav-link" href="{{ url('/') }}">
+                            <img src="{{ asset('images/SG 2.png') }}" alt="S-GUIDE" width="50">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <main class="py-5">
         <div class="container">
             @yield('content')
         </div>
