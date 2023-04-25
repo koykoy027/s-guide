cb3f54fc1f80f229c0b4a17abe390c57e503d325
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-04-25 10:44:20 +0800

    branding url

--- resources/views/Homepage/layout/master.blade.php
+++ resources/views/Homepage/layout/master.blade.php
@@ -21,16 +21,16 @@
             <!-- Navigation-->
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container px-5">
-                    <a class="navbar-brand" href="#"><img src="{{ asset('images/SG 2.png') }}" width="45px" /> - GUIDE</a>
+                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('images/SG 2.png') }}" width="45px" /> - GUIDE</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a href="{{ url('/#top') }}" class="nav-link ">Home</a></li>
+                            <li class="nav-item"><a href="{{ url('/') }}" class="nav-link ">Home</a></li>
                             <li class="nav-item"><a href="{{ url('about') }}" class="nav-link ">About Us</a></li>
                             <li class="nav-item"><a href="{{ url('services') }}" class="nav-link ">Services</a></li>
                             <li class="nav-item"><a href="{{ url('contact') }}" class="nav-link ">Contact Us</a></li>
                             <li class="nav-item"><a class="nav-link" href="{{ url('FAQ') }}">FAQ</a></li>
-                                                    </ul>   
+                        </ul>   
                     </div>
                 </div>
             </nav>
