9c1d55f14a52b1071146d91bb77201362a3510cd
Josh <villanuevajoshua27@gmail.com>
2023-04-25 12:49:57 +0800

    link active fixed

--- resources/views/Homepage/layout/master.blade.php
+++ resources/views/Homepage/layout/master.blade.php
@@ -1,63 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title> S-GUIDE @yield('title')</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <!-- CDN of Link CSS Bootstrap -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body class="d-flex flex-column h-100">
-        <main class="flex-shrink-0">
-            <!-- Navigation-->
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container px-5">
-                    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('images/SG 2.png') }}" width="45px" /> - GUIDE</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a href="{{ url('/') }}" class="nav-link ">Home</a></li>
-                            <li class="nav-item"><a href="{{ url('about') }}" class="nav-link ">About Us</a></li>
-                            <li class="nav-item"><a href="{{ url('services') }}" class="nav-link ">Services</a></li>
-                            <li class="nav-item"><a href="{{ url('contact') }}" class="nav-link ">Contact Us</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ url('FAQ') }}">FAQ</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ url('login') }}">LOGIN</a></li>
-                        </ul>   
-                    </div>
-                </div>
-            </nav>
 
-            @yield('content')
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title> S-GUIDE @yield('title')</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <!-- CDN of Link CSS Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
 
-            
-        </main>
-        <!-- Footer-->
-        <footer class="bg-dark py-4 mt-auto">
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; BigBro 2023</div></div>
-                    <div class="col-auto">
-                        <a class="link-light small" href="#!">Privacy</a>
-                        <span class="text-white mx-1">&middot;</span>
-                        <a class="link-light small" href="#!">Terms</a>
-                        <span class="text-white mx-1">&middot;</span>
-                        <a class="link-light small" href="#!">Contact</a>
-                    </div>
+                <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('images/SG 2.png') }}"
+                        width="45px" /> - GUIDE</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation"><span
+                        class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a href="{{ url('/') }}"
+                                class="nav-link {{ '/' == request()->path() ? 'active' : '' }}">Home</a></li>
+                        <li class="nav-item"><a href="{{ url('about') }}"
+                                class="nav-link {{ 'about' == request()->path() ? 'active' : '' }}">About Us</a></li>
+                        <li class="nav-item"><a href="{{ url('services') }}"
+                                class="nav-link {{ 'services' == request()->path() ? 'active' : '' }}">Services</a></li>
+                        <li class="nav-item"><a href="{{ url('contact') }}"
+                                class="nav-link {{ 'contact' == request()->path() ? 'active' : '' }}">Contact Us</a>
+                        </li>
+                        <li class="nav-item"><a href="{{ url('FAQ') }}"
+                                class="nav-link {{ 'FAQ' == request()->path() ? 'active' : '' }}">FAQ</a></li>
+                        <li class="nav-item"><a href="{{ url('login') }}"
+                                class="nav-link {{ 'login' == request()->path() ? 'active' : '' }}">LOGIN</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-    </body>
+        </nav>
+
+        @yield('content')
+
+
+    </main>
+    <!-- Footer-->
+    <footer class="bg-dark py-4 mt-auto">
+        <div class="container px-5">
+            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                <div class="col-auto">
+                    <div class="small m-0 text-white">Copyright &copy; BigBro 2023</div>
+                </div>
+                <div class="col-auto">
+                    <a class="link-light small" href="#!">Privacy</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">Terms</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">Contact</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
 </html>
