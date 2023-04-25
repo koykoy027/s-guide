c69fa9a70564eff16f42451c6c8e363bc62e8450
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-04-25 10:38:58 +0800

    tagline

--- resources/views/Homepage/services.blade.php
+++ resources/views/Homepage/services.blade.php
@@ -6,7 +6,7 @@
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="{{ asset('images/LandingPage.png') }}" alt="..." /></div>
                 <div class="col-lg-5">
-                    <h1 class="font-weight-light">Business Name or Tagline</h1>
+                    <h1 class="font-weight-light">You're in good hands in S-guide</h1>
                     <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
                     <a class="btn btn-primary" href="#!">Call to Action!</a>
                 </div>
