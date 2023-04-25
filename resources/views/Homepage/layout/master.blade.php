9cbc8529b429f5a004135a17a82cf3f732a6ced6
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-04-25 10:53:00 +0800

    login-frontend

--- resources/views/Homepage/layout/master.blade.php
+++ resources/views/Homepage/layout/master.blade.php
@@ -30,6 +30,7 @@
                             <li class="nav-item"><a href="{{ url('services') }}" class="nav-link ">Services</a></li>
                             <li class="nav-item"><a href="{{ url('contact') }}" class="nav-link ">Contact Us</a></li>
                             <li class="nav-item"><a class="nav-link" href="{{ url('FAQ') }}">FAQ</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('login') }}">LOGIN</a></li>
                         </ul>   
                     </div>
                 </div>
