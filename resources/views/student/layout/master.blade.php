d0ced7d7de5f999d9617ea412c348bdc4273b0df
Josh <villanuevajoshua27@gmail.com>
2023-05-14 11:33:30 +0800

    master in student profile custom js fixed

--- resources/views/student/layout/master.blade.php
+++ resources/views/student/layout/master.blade.php
@@ -59,7 +59,7 @@
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     {{-- custom js --}}
-    <script src="{{ asset('assets/dist/js/script.js') }}"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
     <!-- Bootstrap 4 -->
     <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- ChartJS -->
